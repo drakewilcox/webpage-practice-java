44d221de13323af4c123a77e829c03c590d8b2b0
Duncan Robbins and Drake Wilcox <drake@>
2020-01-14 11:28:50 -0800

    add a function to make the h1 clickable and add border to the page.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,27 @@ $(document).ready(function()  {
   $("#btn1").click(function(){
     $("#col-2").slideDown();
     $("#col-1").slideUp();
-  })
+  });
   $("#btn2").click(function(){
     $("#col-3").slideDown();
     $("#col-2").slideUp();
-  })
+  });
   $("#btn3").click(function(){
     $("#col-1").slideDown();
     $("#col-3").slideUp();
-  })
-})
+  });
+
+  $("#background").click(function(){
+    $("body").addClass("dark");
+    $("#normalize").show();
+    $("#background").hide();
+  });
+  $("#normalize").click(function(){
+    $("body").removeClass("dark");
+    $("#normalize").hide();
+    $("#background").show();
+  });
+  $("#test").click(function(){
+    $("div.col-lg-4").addClass("styling");
+  });
+});
